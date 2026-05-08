5cfbaa9a93f987da90b9b5e80cab92e136fedbbd
Minkh Albert <minkhalbert@yandex.ru>
2026-05-08 09:49:02 +0700

    fixed middleware import again

--- backend/cyber_edu/settings.py
+++ backend/cyber_edu/settings.py
@@ -72,7 +72,7 @@ SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 
 MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'middleware.SessionStatusMiddleware',
+    'cyber_edu.middleware.SessionStatusMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'corsheaders.middleware.CorsMiddleware',
